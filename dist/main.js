0222f9649bb7cd6fddf0a51a6273ac52f08476ff
qzmp10 <loganrobitaille@outlook.com>
2022-08-10 19:35:17 -0400

    first commit

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+alert("hey");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiQUFBQUEsTUFBTSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJhbGVydCgnaGV5Jyk7Il0sIm5hbWVzIjpbImFsZXJ0Il0sInNvdXJjZVJvb3QiOiIifQ==
