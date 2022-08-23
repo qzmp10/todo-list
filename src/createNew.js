a066580bd44d9a45687dc8f853e3c8a51e192213
qzmp10 <loganrobitaille@outlook.com>
2022-08-23 09:55:03 -0400

    test branch

--- src/createNew.js
+++ src/createNew.js
@@ -1,2 +1,6 @@
 
 
+const createNewTodo = function() {
+    add();
+}
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { Todo } from "./todo.js"
-import { Project } from "./projects"
-
+import "./style.css"
+import { Todo } from './todo';
+import { Project } from "./projects";
 
 //restart the whol;e thing dawg
 
